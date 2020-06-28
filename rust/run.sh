4980402b4bd71afd00fae700908f0502cfb44d92
smr97 <saurabh.mraje@gmail.com>
2020-06-28 22:11:50 +0530

    use taskset in Rust; no threadpool

--- rust/run.sh
+++ rust/run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+NUM_THREADS=$2
+FLAG="$(($NUM_THREADS-1))"
 cd $(readlink -f "$(dirname "$0")")
 set -ex
 
-cargo run --release
+echo "******** RUST SORTS ********"
+cargo build --release
+taskset --cpu-list 0-$FLAG cargo run --release $1
--- rust/src/main.rs
+++ rust/src/main.rs
@@ -14,75 +14,54 @@ fn mean_time<F: FnMut() -> std::time::Duration>(mut bench_function: F) -> std::t
         .div_f64(NUM_RUNS as f64)
 }
 macro_rules! bench_sort {
-    ($inp: ident, $sorter: ident) => {
+    ($inp: ident, $sortexp: expr) => {
         mean_time(|| {
             $inp.shuffle(&mut thread_rng());
             let start = Instant::now();
-            $inp.$sorter();
+            $sortexp;
             start.elapsed()
-        })
-    };
-    ($tp: ident, $inp: ident, $sortexp: expr) => {
-        mean_time(|| {
-            $tp.install(|| {
-                $inp.shuffle(&mut thread_rng());
-                let start = Instant::now();
-                $sortexp;
-                start.elapsed()
-            })
         })
     };
 }
 
 fn main() {
     let args = env::args().collect::<Vec<_>>();
-    assert_eq!(args.len(), 3);
-    println!("********** Rust sorts **********");
+    assert_eq!(args.len(), 2);
     let problem_size: u64 = args[1].parse().unwrap();
-    let num_threads: usize = args[2].parse().unwrap();
-    let tp = rayon::ThreadPoolBuilder::new()
-        .num_threads(num_threads)
-        .build()
-        .expect("build pool failed");
     let mut input: Vec<u64> = (0..problem_size).map(|_| rand::random()).collect();
     {
         //Sequential stable sort
-        let measured_time = bench_sort!(input, sort);
+        let measured_time = bench_sort!(input, input.sort());
         println!("seq stable\t{:?}", measured_time);
     }
     {
         //Sequential unstable sort
-        let measured_time = mean_time(|| {
-            input.shuffle(&mut thread_rng());
-            let start = Instant::now();
-            input.sort_unstable();
-            start.elapsed()
-        });
+        let measured_time = bench_sort!(input, input.sort_unstable());
         println!("seq unstable\t{:?}", measured_time);
     }
     {
         //Rayon stable
-        let measured_time = bench_sort!(tp, input, input.par_sort());
+        let measured_time = bench_sort!(input, input.par_sort());
         println!("rayon stable\t{:?}", measured_time);
     }
     {
         //Rayon unstable
-        let measured_time = bench_sort!(tp, input, input.par_sort_unstable());
+        let measured_time = bench_sort!(input, input.par_sort_unstable());
         println!("rayon unstable\t{:?}", measured_time);
     }
     {
         //Rayon Adaptive stable iterator
-        let measured_time = bench_sort!(tp, input, merge_sort_adaptive(&mut input));
+        let measured_time = bench_sort!(input, merge_sort_adaptive(&mut input));
         println!("adaptive iter\t{:?}", measured_time);
     }
     {
         //Try fold slice manual sort
-        let measured_time = bench_sort!(tp, input, slice_par_sort(&mut input));
+        let measured_time = bench_sort!(input, slice_par_sort(&mut input));
         println!("try_fold slice\t{:?}", measured_time);
     }
     {
         //Try fold iterator sort
-        let measured_time = bench_sort!(tp, input, iter_par_sort(&mut input));
+        let measured_time = bench_sort!(input, iter_par_sort(&mut input));
         println!("try_fold iter\t{:?}", measured_time);
     }
 }
