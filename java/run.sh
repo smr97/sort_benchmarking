c6b8771630a39f7e3dcdff25e2702bb7e1f2d502
smr97 <saurabh.mraje@gmail.com>
2020-07-04 23:32:58 +0530

    update scripts for correct cmd line args

--- java/run.sh
+++ java/run.sh
@@ -9,4 +9,9 @@ set -ex
 javac Bench.java
 
 echo ${NUM_THREADS}" threads"
-taskset --cpu-list 0-$FLAG java Bench $1
+if [ $NUM_THREADS == "1" ]
+then
+    taskset --cpu-list 0-$FLAG java Bench $1 1
+else
+    taskset --cpu-list 0-$FLAG java Bench $1 0
+fi
