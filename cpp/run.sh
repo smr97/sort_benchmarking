e8b9e3dc766fb7eaa74d445056fcf57049514188
smr97 <saurabh.mraje@gmail.com>
2020-07-27 18:00:46 +0200

    add tbb path for grid

--- cpp/run.sh
+++ cpp/run.sh
@@ -5,8 +5,7 @@ FLAG="$(($NUM_THREADS-1))"
 cd $(readlink -f "$(dirname "$0")")
 set -ex
 
-#export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/grid5000/spack/opt/spack/linux-debian9-x86_64/gcc-6.4.0/intel-tbb-2019.2-q2t24fbv6s4d5x2j5jaykuqrdkec75zq/lib
-#export LIBRARY_PATH=$LIBRARY_PATH:$LD_LIBRARY_PATH
+source /home/sraje/tbb-2020.3-lin/pstl/bin/pstlvars.sh intel64
 g++ -flto -march=native -std=c++17 -ltbb -fopenmp -O2 bench.cpp -o bench
 
 echo ${NUM_THREADS}" threads"
