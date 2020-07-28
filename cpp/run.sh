05be1402f693098be60858aa13f47afb8d08f631
smr97 <saurabh.mraje@gmail.com>
2020-07-29 01:36:58 +0530

    fix compile error

--- cpp/run.sh
+++ cpp/run.sh
@@ -7,7 +7,7 @@ cd $(readlink -f "$(dirname "$0")")
 set -ex
 
 source /home/sraje/tbb-2020.3-lin/pstl/bin/pstlvars.sh intel64
-g++ -flto -march=native -std=c++17 -ltbb -fopenmp -O2 bench.cpp -o bench
+g++ -flto -march=native -std=c++17 -O2 bench.cpp -ltbb -fopenmp -o bench
 
 echo ${NUM_THREADS}" threads"
 taskset --cpu-list 0-$FLAG ./bench $PROBLEM_SIZE $NUM_THREADS
