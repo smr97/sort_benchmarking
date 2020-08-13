002036d7758402b650bde376b933c947dabe2f06
smr97 <saurabh.mraje@gmail.com>
2020-08-13 18:38:26 +0530

    put back the source for grid

--- cpp/run.sh
+++ cpp/run.sh
@@ -6,13 +6,13 @@ FLAG="$(($NUM_THREADS-1))"
 cd $(readlink -f "$(dirname "$0")")
 set -ex
 
-#source /home/sraje/tbb-2020.3-lin/pstl/bin/pstlvars.sh intel64
+source /home/sraje/tbb-2020.3-lin/pstl/bin/pstlvars.sh intel64
 g++ -flto -march=native -std=c++17 -O2 bench.cpp -ltbb -fopenmp -o bench
 
 echo ${NUM_THREADS}" threads"
 if [ $NUM_THREADS == "1" ]
 then
-    exit 0
+    taskset --cpu-list 0-$FLAG ./bench $PROBLEM_SIZE 1
 else
     taskset --cpu-list 0-$FLAG ./bench $PROBLEM_SIZE $NUM_THREADS
 fi
